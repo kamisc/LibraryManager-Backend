42efbfedaeca3d5d65b8ffbd9d1580c52bb9dc23
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-16 22:24:17 +0200

    Init commit

---
+++ src/main/java/com/sewerynkamil/librarymanager/LibraryManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.librarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerBackendApplication.class, args);
+    }
+
+}
