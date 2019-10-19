5ac46c2566b12b5443abe5a10ed55a2e143d5a13
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-19 12:41:42 +0200

    Add Book entity and Category enum

---
+++ src/main/java/com/sewerynkamil/librarymanager/domain/Book.java
@@ -0,0 +1,55 @@
+package com.sewerynkamil.librarymanager.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(unique = true)
+    private Long id;
+
+    @NotNull
+    @Length(min = 3)
+    private String author;
+
+    @NotNull
+    @Length(min = 2)
+    private String title;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @NotNull
+    private Integer yearOfFirstPublication;
+
+    @NotNull
+    private Integer isbn;
+
+    @NotNull
+    private LocalDate createDate = LocalDate.now();
+
+    public Book(String author, String title, Category category, Integer yearOfFirstPublication, Integer isbn) {
+        this.author = author;
+        this.title = title;
+        this.category = category;
+        this.yearOfFirstPublication = yearOfFirstPublication;
+        this.isbn = isbn;
+    }
+}
