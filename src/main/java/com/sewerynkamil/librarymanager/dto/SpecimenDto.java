9d3fe939f4b179c38d70b6935159546b449edbab
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-20 22:25:28 +0200

    Add SpecimenDto class

---
+++ src/main/java/com/sewerynkamil/librarymanager/dto/SpecimenDto.java
@@ -0,0 +1,28 @@
+package com.sewerynkamil.librarymanager.dto;
+
+import com.sewerynkamil.librarymanager.domain.Book;
+import com.sewerynkamil.librarymanager.domain.enumerated.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class SpecimenDto {
+    private Status status;
+    private String publisher;
+    private Integer yearOfPublication;
+    private String bookTitle;
+
+    public SpecimenDto(Status status, String publisher, Integer yearOfPublication, String bookTitle) {
+        this.status = status;
+        this.publisher = publisher;
+        this.yearOfPublication = yearOfPublication;
+        this.bookTitle = bookTitle;
+    }
+}
