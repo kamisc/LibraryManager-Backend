298c0860eb3d9b5ff31f802ccc138c2078a260a5
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-21 20:59:35 +0200

    Add RentMapper

---
+++ src/main/java/com/sewerynkamil/librarymanager/mapper/RentMapper.java
@@ -0,0 +1,42 @@
+package com.sewerynkamil.librarymanager.mapper;
+
+import com.sewerynkamil.librarymanager.domain.Rent;
+import com.sewerynkamil.librarymanager.dto.RentDto;
+import com.sewerynkamil.librarymanager.repository.SpecimenRepository;
+import com.sewerynkamil.librarymanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@Component
+public class RentMapper {
+    public RentDto mapToRentDto(final Rent rent) {
+        return new RentDto(
+                rent.getId(),
+                rent.getUser().getId(),
+                rent.getSpecimen().getId(),
+                rent.getSpecimen().getBook().getTitle(),
+                rent.getUser().getEmail(),
+                rent.getRentDate(),
+                rent.getReturnDate());
+    }
+
+    public List<RentDto> mapToRentDtoList(final List<Rent> rentList) {
+        return rentList.stream()
+                .map(rent -> new RentDto(
+                        rent.getId(),
+                        rent.getUser().getId(),
+                        rent.getSpecimen().getId(),
+                        rent.getSpecimen().getBook().getTitle(),
+                        rent.getUser().getEmail(),
+                        rent.getRentDate(),
+                        rent.getReturnDate()))
+                .collect(Collectors.toList());
+    }
+}
