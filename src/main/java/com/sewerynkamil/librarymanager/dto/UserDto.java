20f3990e78c155c9e72a84b18c0fabd4680f4a03
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-21 18:14:14 +0200

    Add UserDto class

---
+++ src/main/java/com/sewerynkamil/librarymanager/dto/UserDto.java
@@ -0,0 +1,34 @@
+package com.sewerynkamil.librarymanager.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+    private String userAccountId;
+    private String name;
+    private String surname;
+    private String email;
+    private Integer phoneNumber;
+    private String password;
+
+    public UserDto(Long id, String name, String surname, String email, Integer phoneNumber, String password) {
+        this.userAccountId = generateAccountId(id);
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    private String generateAccountId(Long id) {
+        return ("000000000" + id).substring(id.toString().length() - 1);
+    }
+}
