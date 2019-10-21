429c5fc667e3cf1ad42092e6583f3dbe30472049
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-10-21 18:42:40 +0200

    Add BookMapper

---
+++ src/main/java/com/sewerynkamil/librarymanager/mapper/BookMapper.java
@@ -0,0 +1,44 @@
+package com.sewerynkamil.librarymanager.mapper;
+
+import com.sewerynkamil.librarymanager.domain.Book;
+import com.sewerynkamil.librarymanager.dto.BookDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@Component
+public class BookMapper {
+    public BookDto mapToBookDto(final Book book) {
+        return new BookDto(
+                book.getAuthor(),
+                book.getTitle(),
+                book.getCategory(),
+                book.getYearOfFirstPublication(),
+                book.getIsbn());
+    }
+
+    public Book mapToBook(final BookDto bookDto) {
+        return new Book(
+                bookDto.getAuthor(),
+                bookDto.getTitle(),
+                bookDto.getCategory(),
+                bookDto.getYearOfFirstPublication(),
+                bookDto.getIsbn());
+    }
+
+    public List<BookDto> mapToBookDtoList(final List<Book> bookList) {
+        return bookList.stream()
+                .map(book -> new BookDto(
+                        book.getAuthor(),
+                        book.getTitle(),
+                        book.getCategory(),
+                        book.getYearOfFirstPublication(),
+                        book.getIsbn()))
+                .collect(Collectors.toList());
+    }
+}
