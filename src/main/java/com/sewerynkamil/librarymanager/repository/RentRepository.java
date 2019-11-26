3a645fe213ff48bd0cb7f50ee5be30b622138b5d
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-26 18:25:34 +0100

    Update RentRepository - delete useless imports

--- src/main/java/com/sewerynkamil/librarymanager/repository/RentRepository.java
+++ src/main/java/com/sewerynkamil/librarymanager/repository/RentRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Author Kamil Seweryn
