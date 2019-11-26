5d1abe8a288f765bbd021cab1200adc6801b2821
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-26 18:17:24 +0100

    Update UserRepository - delete useless import

--- src/main/java/com/sewerynkamil/librarymanager/repository/UserRepository.java
+++ src/main/java/com/sewerynkamil/librarymanager/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.sewerynkamil.librarymanager.repository;
 
 import com.sewerynkamil.librarymanager.domain.User;
-import com.sewerynkamil.librarymanager.domain.enumerated.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
