61f2fc5cc4c2d3d311770556502e92f45d792b5d
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-27 18:56:54 +0100

    Update WolneLekturyControllerTestSuite - update testGetAllAudiobooksByTitleStartsWithIgnoreCase() test and delete useless import

--- src/test/java/com/sewerynkamil/librarymanager/controller/WolneLekturyControllerTestSuite.java
+++ src/test/java/com/sewerynkamil/librarymanager/controller/WolneLekturyControllerTestSuite.java
@@ -1,33 +1,26 @@
 package com.sewerynkamil.librarymanager.controller;
 
-import com.sewerynkamil.librarymanager.client.WolneLekturyClient;
-import com.sewerynkamil.librarymanager.config.WolneLekturyConfig;
 import com.sewerynkamil.librarymanager.config.security.AuthenticationEntryPointJwt;
 import com.sewerynkamil.librarymanager.config.security.TokenUtilJwt;
-import com.sewerynkamil.librarymanager.domain.Book;
 import com.sewerynkamil.librarymanager.dto.wolneLektury.WolneLekturyAudiobookDto;
 import com.sewerynkamil.librarymanager.service.UserService;
 import com.sewerynkamil.librarymanager.service.WolneLekturyService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.http.MediaType;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -91,7 +84,7 @@ public class WolneLekturyControllerTestSuite {
         when(wolneLekturyService.fetchAllAudiobooksByTitleStartsWithIgnoreCase(title)).thenReturn(audiobookList);
 
         // When & Then
-        mockMvc.perform(get("/v1/audiobooks/authors/" + title)
+        mockMvc.perform(get("/v1/audiobooks/titles/" + title)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is(200))
                 .andExpect(jsonPath("$", hasSize(0)));
