218fef001c54fcd937afb8901d7a4921eec0bb90
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-27 18:43:05 +0100

    Update UserServiceTestSuite - add 4 new tests

--- src/test/java/com/sewerynkamil/librarymanager/service/UserServiceTestSuite.java
+++ src/test/java/com/sewerynkamil/librarymanager/service/UserServiceTestSuite.java
@@ -12,11 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 import java.util.List;
+
+import static java.util.Optional.ofNullable;
 
 /**
  * Author Kamil Seweryn
@@ -29,24 +30,9 @@ public class UserServiceTestSuite {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private PasswordEncoder bcryptEncoder;
+    /*@Autowired
+    private PasswordEncoder bcryptEncoder;*/
 
-    @Test
-    @Transactional
-    public void testLoadByUsername() throws UserExistException {
-        // Given
-        User user = new User("User", "Surname", "user@gmail.com", 123456789, "1a2b3c4d", Role.ADMIN.getRole());
-        userService.saveUser(user);
-
-        // When
-        UserDetails userDetails = userService.loadUserByUsername(user.getEmail());
-
-        System.out.println(userDetails.getAuthorities().toString());
-        // Then
-        Assert.assertEquals("user@gmail.com", userDetails.getUsername());
-        Assert.assertEquals(1, userDetails.getAuthorities().size());
-    }
 
     @Test
     @Transactional
@@ -62,6 +48,42 @@ public class UserServiceTestSuite {
 
         // Then
         Assert.assertEquals(2, users.size());
+    }
+
+    @Test
+    @Transactional
+    public void testFindAllUsersByNameStartsWithIgnoreCase() throws UserExistException {
+        // Given
+        User user1 = new User("Usr1", "Surname1", "us1@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
+        User user2 = new User("User2", "Surname2", "user2@gmail.com", 234567891, "a1b2c3d4", Role.USER.getRole());
+        userService.saveUser(user1);
+        userService.saveUser(user2);
+
+        // When
+        List<User> usersUs = userService.findAllUsersByNameStartsWithIgnoreCase("us");
+        List<User> usersUse = userService.findAllUsersByNameStartsWithIgnoreCase("use");
+
+        // Then
+        Assert.assertEquals(2, usersUs.size());
+        Assert.assertEquals(1, usersUse.size());
+    }
+
+    @Test
+    @Transactional
+    public void testFindAllUsersBySurnameStartsWithIgnoreCase() throws UserExistException {
+        // Given
+        User user1 = new User("User1", "Suname1", "us1@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
+        User user2 = new User("User2", "Surname2", "user2@gmail.com", 234567891, "a1b2c3d4", Role.USER.getRole());
+        userService.saveUser(user1);
+        userService.saveUser(user2);
+
+        // When
+        List<User> usersSu = userService.findAllUsersBySurnameStartsWithIgnoreCase("su");
+        List<User> usersSur = userService.findAllUsersBySurnameStartsWithIgnoreCase("sur");
+
+        // Then
+        Assert.assertEquals(2, usersSu.size());
+        Assert.assertEquals(1, usersSur.size());
     }
 
     @Test
@@ -131,6 +153,24 @@ public class UserServiceTestSuite {
 
     @Test
     @Transactional
+    public void testUpdateUser() throws UserExistException {
+        // Given
+        User user = new User("User", "Surname", "user@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
+        userService.saveUser(user);
+
+        // When
+        User updatedUser = new User("Use", "Surnam", "use@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
+        updatedUser.setId(user.getId());
+        userService.updateUser(updatedUser);
+
+        // Then
+        Assert.assertEquals("Use", user.getName());
+        Assert.assertEquals("Surnam", user.getSurname());
+        Assert.assertEquals("use@gmail.com", user.getEmail());
+    }
+
+    @Test
+    @Transactional
     public void testDeleteUser() throws UserExistException, UserNotExistException {
         // Given
         User user = new User("User", "Surname", "user@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
@@ -158,6 +198,38 @@ public class UserServiceTestSuite {
         // Then
         Assert.assertTrue(isUserExist);
         Assert.assertFalse(isUserNotExist);
+    }
+
+    @Test
+    @Transactional
+    public void testCountUsers() throws UserExistException {
+        // Given
+        User user1 = new User("User1", "Surname1", "user1@gmail.com", 123456789, "1a2b3c4d", Role.USER.getRole());
+        User user2 = new User("User2", "Surname2", "user2@gmail.com", 987654321, "d4c3bda1", Role.USER.getRole());
+        userService.saveUser(user1);
+        userService.saveUser(user2);
+
+        // When
+        Long count = userService.countUsers();
+
+        // Then
+        Assert.assertEquals(ofNullable(2L).get(), count);
+    }
+
+    @Test
+    @Transactional
+    public void testLoadByUsername() throws UserExistException {
+        // Given
+        User user = new User("User", "Surname", "user@gmail.com", 123456789, "1a2b3c4d", Role.ADMIN.getRole());
+        userService.saveUser(user);
+
+        // When
+        UserDetails userDetails = userService.loadUserByUsername(user.getEmail());
+
+        System.out.println(userDetails.getAuthorities().toString());
+        // Then
+        Assert.assertEquals("user@gmail.com", userDetails.getUsername());
+        Assert.assertEquals(1, userDetails.getAuthorities().size());
     }
 
     @Test(expected = UserNotExistException.class)
