b125ae1d59ee7f782c115890881630341f9a1da4
Kamil Seweryn <kamil_seweryn@o2.pl>
2019-11-28 20:52:58 +0100

    Update NYTimesControllerTestSuite - delete useless imports

--- src/test/java/com/sewerynkamil/librarymanager/controller/NYTimesControllerTestSuite.java
+++ src/test/java/com/sewerynkamil/librarymanager/controller/NYTimesControllerTestSuite.java
@@ -1,36 +1,29 @@
 package com.sewerynkamil.librarymanager.controller;
 
-import com.sewerynkamil.librarymanager.client.NYTimesClient;
 import com.sewerynkamil.librarymanager.config.security.AuthenticationEntryPointJwt;
 import com.sewerynkamil.librarymanager.config.security.TokenUtilJwt;
 import com.sewerynkamil.librarymanager.dto.nytimes.NYTimesResultsDto;
 import com.sewerynkamil.librarymanager.dto.nytimes.NYTimesSection;
 import com.sewerynkamil.librarymanager.dto.nytimes.NYTimesTopStoriesDto;
-import com.sewerynkamil.librarymanager.dto.wolneLektury.WolneLekturyAudiobookDto;
 import com.sewerynkamil.librarymanager.service.NYTimesService;
 import com.sewerynkamil.librarymanager.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.http.MediaType;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
